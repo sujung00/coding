c1f021a2228c635ccd233fbc9c4009f0c235fc20
sujung00 <rlatnwjdirin@naver.com>
2022-07-14 15:19:53 +0900

    4

--- src/Java4/Main.java
+++ src/Java4/Main.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public ArrayList<String> solution(int i, String[] words){
+    public ArrayList<String> solution(String[] words){
         ArrayList<String> answer = new ArrayList<>();
 
-
+        for (String x : words){
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }
 
         return answer;
     }
@@ -15,12 +18,15 @@ public class Main {
     public static void main(String[] args){
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
+
         int count = scanner.nextInt();
         String[] words = new String[count];
+
         for (int i = 0; i<count; i++){
             words[i] = scanner.next();
         }
-        for (String x : T.solution(count, words)){
+
+        for (String x : T.solution(words)){
             System.out.println(x);
         }
     }
