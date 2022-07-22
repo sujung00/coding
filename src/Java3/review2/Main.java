543aa6ea4fb74e7714116a8d472dc8f63fb3f175
sujung00 <rlatnwjdirin@naver.com>
2022-07-22 22:21:10 +0900

    5, 6 복습

---
+++ src/Java3/review2/Main.java
@@ -0,0 +1,27 @@
+package Java3.review2;
+
+import java.util.Scanner;
+
+public class Main {
+    public String solution(String str){
+        String answer = "";
+        String[] words = str.split(" ");
+
+        for (String x : words){
+            if (x.length() > answer.length()){
+                answer = x;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.nextLine();
+
+        System.out.println(T.solution(str));
+    }
+}
