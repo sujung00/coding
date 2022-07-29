177268487750e7fea3c7640d5ca1dad155d5a856
sujung00 <rlatnwjdirin@naver.com>
2022-07-30 03:02:28 +0900

    19

---
+++ src/Java19/Main.java
@@ -0,0 +1,29 @@
+package Java19;
+
+import java.util.Scanner;
+
+public class Main {
+    public int solution(int n, int[] arr){
+        int answer = 0, cnt = 0;
+        for (int i = 0; i < n; i++){
+            if (arr[i] == 1) {
+                cnt++;
+                answer += cnt;
+            }
+            else cnt = 0;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(T.solution(n, arr));
+    }
+}
