8d2e08794188df31d13ba36a3f35f53524ff6c7c
sujung00 <rlatnwjdirin@naver.com>
2022-08-04 23:32:54 +0900

    23

---
+++ src/Java22/Main.java
@@ -0,0 +1,14 @@
+package Java22;
+
+import java.util.Scanner;
+
+/*public class Main {
+    public
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}*/
