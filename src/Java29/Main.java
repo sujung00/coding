8efc33f2d506e463b4fab7cd66c191c330b9f779
sujung00 <rlatnwjdirin@naver.com>
2022-08-14 23:33:39 +0900

    29

---
+++ src/Java29/Main.java
@@ -0,0 +1,35 @@
+package Java29;
+
+import java.util.Scanner;
+
+public class Main {
+    public int solution(int n, int m, int[] arr){
+        int answer = 0, sum = 0, lt = 0;
+
+        for (int rt = 0; rt < n; rt++){
+            sum+=arr[rt];
+            if (sum == m) answer++;
+            while (sum >= m){
+                sum -= arr[lt++];
+                if(sum == m) answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++){
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println(T.solution(n, m, arr));
+    }
+}
