6f81ed6f8073edd4a10dd4a8cd669267a5ad1ee6
sujung00 <rlatnwjdirin@naver.com>
2022-08-19 19:40:09 +0900

    29

--- src/Java28/Main.java
+++ src/Java28/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public int solution(int n, int k, int[] arr){
         int answer, sum = 0;
+
         for (int i = 0; i < k; i++) sum += arr[i];
         answer = sum;
         for (int i = k; i < n; i++){
@@ -21,8 +22,8 @@ public class Main {
 
         int n = scanner.nextInt();
         int k = scanner.nextInt();
-
         int[] arr = new int[n];
+
         for (int i = 0; i < n; i++){
             arr[i] = scanner.nextInt();
         }
