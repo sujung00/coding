e7e9728da2c8d31951bcac105c69023a4a117366
sujung00 <rlatnwjdirin@naver.com>
2022-08-28 23:52:34 +0900

    38

---
+++ src/Java38/Main.java
@@ -0,0 +1,29 @@
+package Java38;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public String solution(String str){
+        String answer = "";
+        Stack<Character> stack = new Stack<>();
+        for (char x : str.toCharArray()){
+            if (x == ')'){
+                while (stack.pop()!='(');
+            }
+            else stack.push(x);
+        }
+        for (int i = 0; i < stack.size(); i++) answer+=stack.get(i);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.next();
+
+        System.out.println(T.solution(str));
+    }
+}
