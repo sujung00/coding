8bcbb3ab7f892233d6114e97009beafd35d27536
sujung00 <rlatnwjdirin@naver.com>
2022-08-31 20:55:06 +0900

    41

---
+++ src/Java41/Main.java
@@ -0,0 +1,29 @@
+package Java41;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public int solution(String str){
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i<str.length(); i++){
+            if (str.charAt(i) == '(') stack.push('(');
+            else {
+                stack.pop();
+                if (str.charAt(i-1) == '(') answer += stack.size();
+                else answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner scanner= new Scanner(System.in);
+
+        String str = scanner.next();
+        System.out.println(T.solution(str));
+    }
+}
