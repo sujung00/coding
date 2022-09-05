ab9363cf1ce49f274b116d9936edfc2aaf3ae923
sujung00 <rlatnwjdirin@naver.com>
2022-09-05 21:05:40 +0900

    39, 40 복습

--- src/Java39/Main.java
+++ src/Java39/Main.java
@@ -7,20 +7,22 @@ public class  Main {
     public int solution(int[][] board, int[] moves){
         int answer = 0;
         Stack<Integer> stack = new Stack<>();
-        for (int pos : moves){
+
+        for(int pos : moves){
             for (int i = 0; i < board.length; i++){
-                if (board[i][pos-1]!=0){
+                if (board[i][pos-1] != 0){
                     int tmp = board[i][pos-1];
                     board[i][pos-1] = 0;
                     if (!stack.isEmpty() && tmp == stack.peek()){
                         answer += 2;
                         stack.pop();
                     }
-                    else stack.push(tmp);
+                    else {
+                        stack.push(tmp);
+                    }
                     break;
                 }
             }
-
         }
 
         return answer;
--- src/Java40/Main.java
+++ src/Java40/Main.java
@@ -6,14 +6,14 @@ import java.util.Stack;
 public class Main {
     public int solution(String str){
         int answer = 0;
-
         Stack<Integer> stack = new Stack<>();
+
         for (char x : str.toCharArray()){
             if (Character.isDigit(x)) stack.push(x-48);
             else {
                 int rt = stack.pop();
                 int lt = stack.pop();
-                if (x == '+') stack.push(lt+rt);
+                if (x == '+') stack.push(lt + rt);
                 else if (x == '-') stack.push(lt - rt);
                 else if (x == '*') stack.push(lt * rt);
                 else if (x == '/') stack.push(lt / rt);
