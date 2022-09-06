265a1321b9fff17429046f6181fb54fa7631a9a2
sujung00 <rlatnwjdirin@naver.com>
2022-09-06 16:56:41 +0900

    41 복습

--- src/Java41/Main.java
+++ src/Java41/Main.java
@@ -7,7 +7,8 @@ public class Main {
     public int solution(String str){
         int answer = 0;
         Stack<Character> stack = new Stack<>();
-        for (int i = 0; i<str.length(); i++){
+
+        for (int i = 0; i < str.length(); i++){
             if (str.charAt(i) == '(') stack.push('(');
             else {
                 stack.pop();
