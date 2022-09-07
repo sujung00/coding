c77520e5e9363bbf8fb6a3a7f1c3c2c62ed1a13b
sujung00 <rlatnwjdirin@naver.com>
2022-09-07 22:06:10 +0900

    43

---
+++ src/Java43/Main.java
@@ -0,0 +1,31 @@
+package Java43;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public String solution(String need, String plan){
+        String answer = "YES";
+        Queue<Character> Q = new LinkedList<>();
+        for (char x : need.toCharArray()) Q.offer(x);
+        for (char x : plan.toCharArray()){
+            if (Q.contains(x)){
+                if (x != Q.poll()) return "NO";
+            }
+        }
+        if (!Q.isEmpty()) return "NO";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner scanner = new Scanner(System.in);
+
+        String a = scanner.next();
+        String b = scanner.next();
+
+        System.out.println(T.solution(a, b));
+    }
+}
