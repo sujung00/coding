7331563bb5f164cd8761b99b8c5a8f3809d4c6a7
sujung00 <rlatnwjdirin@naver.com>
2022-09-12 23:38:21 +0900

    46

---
+++ src/Java46/Main.java
@@ -0,0 +1,29 @@
+package Java46;
+
+import java.util.Scanner;
+
+public class Main {
+    public int[] solution(int n, int[] arr){
+        for (int i = 0; i < n-1; i++){
+            for (int j = 0; j < n-i-1; j++){
+                if (arr[j] > arr[j+1]){
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = scanner.nextInt();
+        for (int x : T.solution(n, arr)) System.out.print(x + " ");
+    }
+}
