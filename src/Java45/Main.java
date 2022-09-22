3a158a17c2f1645c21e161e16cf209e35c4056d6
sujung00 <rlatnwjdirin@naver.com>
2022-09-22 13:34:26 +0900

    45 복습

--- src/Java45/Main.java
+++ src/Java45/Main.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 public class Main {
     public int[] solution(int n, int[] arr){
+
+        int idx, tmp;
         for (int i = 0; i < n-1; i++){
-            int idx = i;
+            idx = i;
             for (int j = i+1; j < n; j++){
                 if (arr[j] < arr[idx]) idx = j;
             }
-            int tmp = arr[i];
+            tmp = arr[i];
             arr[i] = arr[idx];
             arr[idx] = tmp;
         }
-
         return arr;
     }
 
