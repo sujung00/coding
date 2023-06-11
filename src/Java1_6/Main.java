32f0cc17e3982ce80cacaa3e2a29a178b2e25bde
sujung00 <rlatnwjdirin@naver.com>
2023-06-11 23:21:20 +0900

    01-06

--- src/Java1_6/Main.java
+++ src/Java1_6/Main.java
@@ -2,15 +2,13 @@ package Java1_6;
 
 import java.util.Scanner;
 
-//중복문자제거
+// 2023.06.11
 public class Main {
     public String solution(String str){
         String answer = "";
 
         for (int i = 0; i < str.length(); i++){
-            if (i == str.indexOf(str.charAt(i))){
-                answer += str.charAt(i);
-            }
+            if (str.indexOf(str.charAt(i))==i) answer += str.charAt(i);
         }
 
         return answer;
