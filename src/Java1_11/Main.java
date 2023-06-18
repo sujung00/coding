85e2a71fc7da6492d6426d02c872806da46dff4f
sujung00 <rlatnwjdirin@naver.com>
2023-06-18 21:54:13 +0900

    01-11, 01-12

--- src/Java1_11/Main.java
+++ src/Java1_11/Main.java
@@ -2,26 +2,32 @@ package Java1_11;
 
 import java.util.Scanner;
 
+// 2023.06.18
 public class Main {
-    public String solution(String str) {
+    public String solution(String str){
         String answer = "";
 
-        str = str + " ";
+        char[] c = new char[str.length()+1];
+        for (int i = 0; i < str.length(); i++){
+            c[i] = str.charAt(i);
+        }
+        c[str.length()] = ' ';
+
         int n = 1;
-        for (int i = 0; i < str.length()-1; i++) {
-            if (str.charAt(i) == str.charAt(i+1)){
+        for (int i = 0; i < c.length-1; i++){
+            if(c[i] == c[i+1]){
                 n++;
             } else {
-                answer += str.charAt(i);
-                if (n != 1) {
+                answer += c[i];
+                if (n > 1){
                     answer += n;
-                    n = 1;
                 }
+                n = 1;
             }
         }
 
         return answer;
-    }
+   }
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
--- src/Java1_12/Main.java
+++ src/Java1_12/Main.java
@@ -2,16 +2,20 @@ package Java1_12;
 
 import java.util.Scanner;
 
+// 2023.06.18 ^
 public class Main {
-    public String solution(int n, String str) {
+    public String solution(int num, String str){
         String answer = "";
 
-        str = str.replaceAll("#", "1").replaceAll("[*]", "0");
-
-        for (int i = 0; i < n; i++) {
-            String s = str.substring(str.length()/n*i, str.length()/n*(i+1));
-            int c = Integer.valueOf(s, 2);
-            answer += (char)c ;
+        str = str.replaceAll("#", String.valueOf(1)).replaceAll("[*]", String.valueOf(0));
+        for (int i = 0; i < num; i++){
+            // #****##(0~6) => 1000011 => C
+            // #**####(7~13) => 1001111 => O
+            // #**####(14~20) => 1001111 => O
+            // #**##**(21~27) => 1001100 => L
+            String s = str.substring(i*7, (i+1)*7); // 1000011
+            char c = (char)Integer.parseInt(s, 2);
+            answer += c;
         }
 
         return answer;
@@ -19,8 +23,8 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        int num = scanner.nextInt();
         String str = scanner.next();
-        System.out.println(T.solution(n, str));
+        System.out.println(T.solution(num, str));
     }
 }
