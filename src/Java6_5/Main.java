171368e1964192507d3fb53c4252720a0eb622ad
sujung00 <rlatnwjdirin@naver.com>
2023-07-24 21:52:00 +0900

    06-05

--- src/Java6_5/Main.java
+++ src/Java6_5/Main.java
@@ -7,14 +7,14 @@ import java.util.Scanner;
 public class Main {
     public String solution(int n, int[] arr){
         String answer = "U";
+
         Arrays.sort(arr);
-        for (int i = 0; i < n-1; i++){
-            if(arr[i] == arr[i+1]) return "D";
+        for (int i = 0; i < n; i++){
+            if (arr[i] == arr[i+1]) return "D";
         }
 
         return answer;
     }
-
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
