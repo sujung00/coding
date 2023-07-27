964a6a187a1890f3466679db4138ceac85a5803b
sujung00 <rlatnwjdirin@naver.com>
2023-07-27 23:17:09 +0900

    06-07 복습

--- src/Java6_7/Main.java
+++ src/Java6_7/Main.java
@@ -1,9 +1,39 @@
 package Java6_7;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
+import java.awt.*;
+import java.util.*;
 
+class Point implements Comparable<Point> {
+    int x;
+    int y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if (this.x == o.x){
+            return this.y-o.y;
+        } else {
+            return this.x-o.x;
+        }
+    }
+}
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        ArrayList<Point> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++){
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            arr.add(new Point(x, y));
+        }
+        Collections.sort(arr);
+        for (Point o : arr){
+            System.out.println(o.x + " " + o.y);
+        }
+    }
 }
