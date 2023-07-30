a90ddb7b4f2c0248e2f8e74a0cd6b7b1fcf6d4f0
sujung00 <rlatnwjdirin@naver.com>
2023-07-30 21:14:39 +0900

    06-09

--- src/Java52/Main.java
+++ src/Java52/Main.java
@@ -4,32 +4,45 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    public int count(int[] arr, int capacity){
+        int cnt = 1, sum  = 0;
+        for (int x : arr){
+            if(sum+x > capacity){
+                cnt++;
+                sum = x;
+            } else {
+                sum += x;
+            }
+        }
+
+        return cnt;
+    }
     public int solution(int n, int m, int[] arr){
         int answer = 0;
 
-        Arrays.sort(arr);
-        int lt = 0, rt = n-1;
+        int lt = Arrays.stream(arr).max().getAsInt();
+        int rt = Arrays.stream(arr).sum();
         while (lt <= rt){
             int mid = (lt+rt)/2;
-            if (arr[mid] == m){
-                answer = mid+1;
-                break;
+            if (count(arr, mid) <= m){
+                answer = mid;
+                rt = mid-1;
+            } else {
+                lt = mid+1;
             }
-            if (arr[mid] > m) rt = mid-1;
-            else lt = mid+1;
         }
 
         return answer;
     }
-
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
-
         int n = scanner.nextInt();
         int m = scanner.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) arr[i] = scanner.nextInt();
+        for (int i = 0; i < n; i++){
+            arr[i] = scanner.nextInt();
+        }
         System.out.println(T.solution(n, m, arr));
     }
 }
