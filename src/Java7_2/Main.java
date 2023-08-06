6852b2c22208170337f8f28f9dc1ad4710e6dbe1
sujung00 <rlatnwjdirin@naver.com>
2023-08-06 14:21:40 +0900

    07-02

---
+++ src/Java7_2/Main.java
@@ -0,0 +1,17 @@
+package Java7_2;
+
+import java.util.Scanner;
+
+public class Main {
+    public void DFS(int n){
+        if (n==0) return;
+        else {
+            DFS(n/2);
+            System.out.print(n%2+" ");
+        }
+    }
+    public static void main(String[] args) {
+        Main T = new Main();
+        T.DFS(11);
+    }
+}
