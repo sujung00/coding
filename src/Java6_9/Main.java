f73df31263e3073aac907116dff6d1df6ebad198
sujung00 <rlatnwjdirin@naver.com>
2023-08-11 22:52:37 +0900

    06-09 복습

--- src/Java6_9/Main.java
+++ src/Java6_9/Main.java
@@ -6,25 +6,27 @@ import java.util.Scanner;
 public class Main {
     public int count(int[] arr, int capacity){
         int cnt = 1, sum = 0;
+
         for (int x : arr){
             if (sum+x > capacity){
                 cnt++;
-                sum = x;
+                sum=x;
             } else {
-                sum += x;
+                sum+=x;
             }
         }
+
         return cnt;
     }
-
     public int solution(int n, int m, int[] arr){
         int answer = 0;
 
         int lt = Arrays.stream(arr).max().getAsInt();
         int rt = Arrays.stream(arr).sum();
-        while (lt <= rt){
+
+        while(lt <= rt){
             int mid = (lt+rt)/2;
-            if (count(arr, mid) <= m){
+            if(count(arr, mid) <= m){
                 answer = mid;
                 rt = mid-1;
             } else {
@@ -34,7 +36,6 @@ public class Main {
 
         return answer;
     }
-
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
