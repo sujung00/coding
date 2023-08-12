6652c23dcdbc47d6163588ca3d1ea0b4494150ef
sujung00 <rlatnwjdirin@naver.com>
2023-08-12 09:58:36 +0900

    06-10 복습

--- src/Java6_10/Main.java
+++ src/Java6_10/Main.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 public class Main {
     public int count(int[] arr, int dist){
         int cnt = 1;
-        int ep = arr[0];
 
+        int ep = arr[0];
         for (int i = 1; i < arr.length; i++){
             if (arr[i]-ep >= dist){
                 cnt++;
@@ -17,16 +17,16 @@ public class Main {
 
         return cnt;
     }
-    public int solution(int n, int c, int[] arr){
+
+    public int solution(int n, int m, int[] arr){
         int answer = 0;
 
         Arrays.sort(arr);
         int lt = 1;
         int rt = arr[n-1];
-
         while (lt <= rt){
             int mid = (lt+rt)/2;
-            if (count(arr, mid) >= c){
+            if(count(arr, mid) >= m){
                 answer = mid;
                 lt = mid+1;
             } else {
@@ -36,14 +36,15 @@ public class Main {
 
         return answer;
     }
-
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) arr[i] = scanner.nextInt();
+        for (int i = 0; i < n; i++){
+            arr[i] = scanner.nextInt();
+        }
         System.out.println(T.solution(n, m, arr));
     }
 }
