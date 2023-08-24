05537a8b689abfb4aa74116503d1133d0b22e2fa
sujung00 <rlatnwjdirin@naver.com>
2023-08-24 14:46:03 +0900

    7-11

---
+++ src/Java7_11/Main.java
@@ -0,0 +1,15 @@
+package Java7_11;
+
+// 1. 무방향 그래프
+// 2. 양방향 그래프
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner scanner = new Scanner(System.in);
+        int v = scanner.nextInt();
+        int e = scanner.nextInt();
+    }
+}
