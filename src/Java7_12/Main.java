f4c0d364a15099b2c7aef415b1f82023cdc1dc96
sujung00 <rlatnwjdirin@naver.com>
2023-08-30 21:17:39 +0900

    7-12 복습

--- src/Java7_12/Main.java
+++ src/Java7_12/Main.java
@@ -6,18 +6,20 @@ public class Main {
     static int n, m, answer = 0;
     static int[][] graph;
     static int[] ch;
+
     public void DFS(int v){
-        if (v==n) answer++;
+        if (v == n) answer++;
         else {
             for (int i = 1; i <= n; i++){
                 if (graph[v][i]==1 && ch[i]==0){
-                    ch[i]=1;
+                    ch[i] = 1;
                     DFS(i);
-                    ch[i]=0;
+                    ch[i] = 0;
                 }
             }
         }
     }
+
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
@@ -30,7 +32,7 @@ public class Main {
             int b = scanner.nextInt();
             graph[a][b] = 1;
         }
-        ch[1]=1;
+        ch[1] = 1;
         T.DFS(1);
         System.out.println(answer);
     }
