d4c5be7b3802a47fdbfb3bab12fe85ebf87db7da
sujung00 <rlatnwjdirin@naver.com>
2023-09-14 22:19:09 +0900

    7-6 복습

--- src/Java7_6/Main.java
+++ src/Java7_6/Main.java
@@ -19,18 +19,15 @@ public class Main {
     static int[] ch;
     public void DFS(int L){
         if (L == n+1){
-            String tmp = "";
-            for (int i = 1; i <= n; i++){
-                if (ch[i] == 1) tmp += (i + " ");
-            }
-            if (tmp.length() > 0){
-                System.out.println(tmp);
-            }
+             for (int i = 1; i < ch.length; i++){
+                 if (ch[i] == 1)System.out.print(i + " ");
+             }
+            System.out.println();
         } else {
-            ch[L] = 1;
-            DFS(L+1); // 왼쪽으로 뻗기
-            ch[L] = 0;
-            DFS(L+1); // 오른쪽으로 뻗기
+            ch[L] = 1; // 사용한다
+            DFS(L+1); // 왼쪽으로 뻗기(사용한다)
+            ch[L] = 0; // 사용하지 않는다
+            DFS(L+1); // 오른쪽으로 뻗기(사용하지 않는다)
         }
     }
 
