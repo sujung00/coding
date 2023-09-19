4796b31f186b50f299542fd8bcd2d12cec8b9e92
sujung00 <rlatnwjdirin@naver.com>
2023-09-19 20:27:38 +0900

    7-8 복습

--- src/Java7_8/Main.java
+++ src/Java7_8/Main.java
@@ -5,10 +5,10 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-    int[] dis = {1, -1, 5};
+    int answer = 0;
+    int[] dis = {-1, 0, 5};
     int[] ch;
     Queue<Integer> Q = new LinkedList<>();
-
     public int BFS(int s, int e){
         ch = new int[10001];
         ch[s] = 1;
@@ -18,12 +18,10 @@ public class Main {
             int len = Q.size();
             for (int i = 0; i < len; i++){
                 int x = Q.poll();
+                if (x == e) return L;
                 for (int j = 0; j < 3; j++){
                     int nx = x + dis[j];
-                    if (nx == e){
-                        return L+1;
-                    }
-                    if (nx >= 1 && nx < 10000 && ch[nx]==0){
+                    if (nx >= 1 && nx <= 10000 && ch[nx] == 0){
                         ch[nx] = 1;
                         Q.offer(nx);
                     }
@@ -32,7 +30,7 @@ public class Main {
             L++;
         }
 
-        return L;
+        return 0;
     }
     public static void main(String[] args) {
         Main T = new Main();
