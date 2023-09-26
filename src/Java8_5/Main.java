7dfef2cfec550b98404835e6415e80f088f35b39
sujung00 <rlatnwjdirin@naver.com>
2023-09-26 10:06:20 +0900

    8-5 runtime error 수정

--- src/Java8_5/Main.java
+++ src/Java8_5/Main.java
@@ -1,13 +1,16 @@
 package Java8_5;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 // 동전교환
 public class Main {
     static int n, m, answer = Integer.MAX_VALUE;
 
-    public void DFS(int L, int sum, int[] arr){
+    public void DFS(int L, int sum, Integer[] arr){
         if (sum > m) return;
+        if (L >= answer) return;
         if (sum == m) {
             answer = Math.min(answer, L);
         }
@@ -22,10 +25,11 @@ public class Main {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
         n = scanner.nextInt();
-        int[] arr = new int[n];
+        Integer[] arr = new Integer[n];
         for (int i = 0; i < n; i++){
             arr[i] = scanner.nextInt();
         }
+        Arrays.sort(arr, Collections.reverseOrder());
         m = scanner.nextInt();
         T.DFS(0,0, arr);
         System.out.println(answer);
