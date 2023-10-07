7a4de75c5dcd5c5c61955ea9cc05085fad188c62
sujung00 <rlatnwjdirin@naver.com>
2023-10-07 23:18:47 +0900

    7-14 복습

--- src/Java7_14/Main.java
+++ src/Java7_14/Main.java
@@ -5,10 +5,21 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
+//6 9
+//1 3
+//1 4
+//2 1
+//2 5
+//3 4
+//4 5
+//4 6
+//6 2
+//6 5
+
 public class Main {
-    static int n, m;
-    static int[] ch, dis;
     static ArrayList<ArrayList<Integer>> graph;
+    static int[] dis, ch;
+    static int n, m;
 
     public void BFS(int v){
         Queue<Integer> Q = new LinkedList<>();
@@ -21,29 +32,30 @@ public class Main {
                 if (ch[nv] == 0){
                     ch[nv] = 1;
                     Q.offer(nv);
-                    dis[nv] = dis[cv]+1;
+                    dis[nv] = dis[cv] + 1;
                 }
             }
         }
     }
+
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
         n = scanner.nextInt();
         m = scanner.nextInt();
+        ch = new int[n+1];
+        dis = new int[n+1];
         graph = new ArrayList<ArrayList<Integer>>();
         for (int i = 0; i <= n; i++){
             graph.add(new ArrayList<Integer>());
         }
-        ch = new int[n+1];
-        dis = new int[n+1];
         for (int i = 0; i < m; i++){
             int a = scanner.nextInt();
             int b = scanner.nextInt();
             graph.get(a).add(b);
         }
         T.BFS(1);
-        for (int i = 2; i <= n; i++){
+        for (int i = 1; i <= n; i++){
             System.out.println(i + " : " + dis[i]);
         }
     }
