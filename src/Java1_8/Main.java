c92ec508a21045afb59d7fcbf6c564d8964550f6
sujung00 <rlatnwjdirin@naver.com>
2023-10-29 10:03:40 +0900

    1-8 복습

--- src/Java1_8/Main.java
+++ src/Java1_8/Main.java
@@ -5,27 +5,15 @@ import java.util.Scanner;
 // 2023.06.15
 public class Main {
     public String solution(String str){
-        String answer = "YES";
+        String answer = "NO";
 
-        str = str.toUpperCase();
-        int lt = 0, rt = str.length()-1;
-        while (lt < rt){
-            if (Character.isAlphabetic(str.charAt(lt))){
-                if (Character.isAlphabetic(str.charAt(rt))){
-                    // 양쪽 다 알파벳이면
-                    if (str.charAt(lt) != str.charAt(rt)){
-                        answer = "NO";
-                        return answer;
-                    } else {
-                        lt++;
-                        rt--;
-                    }
-                } else rt--;
-            } else lt++;
-        }
+        str = str.toUpperCase().replaceAll("[^A-Z]", "");
+        String tmp = new StringBuilder(str).reverse().toString();
+        if (str.equals(tmp)) answer = "YES";
 
         return answer;
     }
+
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
