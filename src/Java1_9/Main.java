0f1152722079cc761134ebeffb7e079213fd82d1
sujung00 <rlatnwjdirin@naver.com>
2023-10-30 22:32:36 +0900

    1-9 복습

--- src/Java1_9/Main.java
+++ src/Java1_9/Main.java
@@ -7,14 +7,16 @@ public class Main {
     public int solution(String str){
         String answer = "";
 
-        for (int i = 0; i < str.length(); i++){
-            if (!Character.isAlphabetic(str.charAt(i))){
-                answer += str.charAt(i);
+        char[] arr = str.toCharArray();
+        for (int i = 0; i < str.length()-1; i++){
+            if (!Character.isAlphabetic(arr[i])){
+                answer += arr[i];
             }
         }
 
         return Integer.parseInt(answer);
     }
+
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
