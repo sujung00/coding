2a6bc30b9ce3178640e3665eccd03658250b6de7
sujung00 <rlatnwjdirin@naver.com>
2023-11-01 22:33:42 +0900

    9-6

--- src/Java1_10/Main.java
+++ src/Java1_10/Main.java
@@ -4,20 +4,19 @@ import java.util.Scanner;
 
 // 2023.06.17*
 public class Main {
-    public int[] solution(String str, char t){
+    public int[] solution(String str, char c){
         int[] answer = new int[str.length()];
+
         int p = 1000;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == t){
+        for (int i = 0; i < str.length(); i++){
+            if (str.charAt(i) == c) {
                 p = 0;
-            } else {
-                p++;
-            }
+            } else p++;
             answer[i] = p;
         }
         p = 1000;
         for (int i = str.length()-1; i >= 0; i--){
-            if (str.charAt(i) == t){
+            if (str.charAt(i) == c) {
                 p = 0;
             } else {
                 p++;
@@ -31,8 +30,8 @@ public class Main {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
         String str = scanner.next();
-        char t = scanner.next().charAt(0);
-        for (int x : T.solution(str, t)) {
+        char c = scanner.next().charAt(0);
+        for (int x : T.solution(str, c)){
             System.out.print(x + " ");
         }
     }
--- src/Java9_6/Main.java
+++ src/Java9_6/Main.java
@@ -3,8 +3,33 @@ package Java9_6;
 import java.util.Scanner;
 
 public class Main {
+    static int[] unf;
+    public static int Find(int v){
+        if (v==unf[v]) return v;
+        else return unf[v] = Find(unf[v]);
+    }
+    public static void Union(int a, int b){
+        int fa = Find(a);
+        int fb = Find(b);
+        if (fa != fb) unf[fa] = fb;
+    }
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        unf = new int[n+1];
+        for (int i = 1; i <= n; i++) unf[i] = i;
+        for (int i = 1; i <= m; i++){
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            Union(a, b);
+        }
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int fa = Find(a);
+        int fb = Find(b);
+        if (fa == fb) System.out.println("YES");
+        else System.out.println("NO");
     }
 }
