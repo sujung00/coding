48bab04a0c84abe7606d389d7514d138f77fbd83
sujung00 <rlatnwjdirin@naver.com>
2023-11-22 15:36:17 +0900

    3-6 복습

--- src/Java3_6/Main.java
+++ src/Java3_6/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Main {
     public int solution(int n, int k, int[] arr){
         int answer = 0, cnt = 0, lt = 0;
+
         for (int rt = 0; rt < n; rt++){
             if (arr[rt] == 0) cnt++;
             while (cnt > k){
@@ -17,6 +18,7 @@ public class Main {
 
         return answer;
     }
+
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
