2e8f6c044f657dfdacbafe1d059ec110744e6b8e
sujung00 <rlatnwjdirin@naver.com>
2023-12-02 23:18:00 +0900

    3-5 복습

--- src/Java3_5/Main.java
+++ src/Java3_5/Main.java
@@ -6,19 +6,18 @@ import java.util.Scanner;
 public class Main {
     public int solution(int n){
         int answer = 0;
-        int lt = 0, sum = 0;
 
-        int m = n/2+1;
-        int[] arr = new int[m];
-        for (int i = 0; i < m; i++){
-            arr[i] = i+1;
-        }
-        for (int rt = 0; rt < m; rt++){
-            sum += arr[rt];
-            if (sum == n) answer++;
+        int lt = 0, sum = 0;
+        for (int rt = 0; rt < n; rt++){
+            sum += rt;
             while (sum >= n){
-                sum -= arr[lt++];
-                if (sum == n) answer++;
+                if (sum == n) {
+                    answer++;
+                    break;
+                }
+                else {
+                    sum -= lt++;
+                }
             }
         }
 
