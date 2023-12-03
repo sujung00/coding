c8e3723b4ffa20337dc5df2e067d7e579129f624
sujung00 <rlatnwjdirin@naver.com>
2023-12-03 23:55:42 +0900

    3-6 복습

--- src/Java3_6/Main.java
+++ src/Java3_6/Main.java
@@ -6,14 +6,19 @@ import java.util.Scanner;
 public class Main {
     public int solution(int n, int k, int[] arr){
         int answer = 0;
+        int lt = 0, cnt = 0;
 
-        int cnt = 0, lt = 0;
         for (int rt = 0; rt < n; rt++){
-            if (arr[rt] == 0) cnt++;
+            if (arr[rt] == 0) {
+                cnt++;
+            }
             while (cnt > k){
-                if (arr[lt] == 0) cnt--;
+                if (arr[lt] == 0) {
+                    cnt--;
+                }
                 lt++;
             }
+
             answer = Math.max(answer, rt-lt+1);
         }
 
